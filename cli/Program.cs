7f2516fbbef40803d963fed76577586844f2fd6b
jan.fojtik <jan.fojtik@indicia.nl>
2023-05-10 17:24:25 +0200

    breaking changes, logic for FEN interpretation has to be fixed, however visualization works, extraction of game data will follow

---
+++ cli/Program.cs
@@ -0,0 +1,9 @@
+﻿using logic;
+
+//4kb1r/p1pRqppp/1p6/8/b4NP1/4B2P/PP3P2/5RK1 w k - 0 21
+
+Console.WriteLine("Please enter a FEN code: ");
+string? FEN = Console.Read();
+FENInterpreter.LoadPositionFromFEN(FEN ?? "");
+
+BoardVisualizer.VisualizeBoardFromFEN();
