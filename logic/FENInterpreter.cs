96dd30fef52bc1ab4bd63bde0d9049fe4201f13c
jan.fojtik <jan.fojtik@indicia.nl>
2023-05-11 15:39:59 +0200

    addressed comments

--- logic/FENInterpreter.cs
+++ logic/FENInterpreter.cs
@@ -8,40 +8,34 @@ public class FENInterpreter
 {
     public static bool IsValidFEN(string fen)
     {
-        try
+        string[] fenParts = fen.Split(' ');
+
+        if (fenParts.Length != 6)
         {
-            string[] fenParts = fen.Split(' ');
-
-            if (fenParts.Length != 6)
-            {
-                return false;
-            }
-
-            string position = fenParts[0];
-            string activeColor = fenParts[1];
-            string castlingAvailability = fenParts[2];
-            string enPassantTargetSquare = fenParts[3];
-            string halfMoveClock = fenParts[4];
-            string fullMoveNumber = fenParts[5];
-
-            Regex positionPattern = new Regex(@"^([rnbqkpRNBQKP1-8]{1,8}\/){7}[rnbqkpRNBQKP1-8]{1,8}$");
-            Regex activeColorPattern = new Regex("^[bw]$");
-            Regex castlingAvailabilityPattern = new Regex(@"^((K?Q?k?q?)|-)$");
-            Regex enPassantTargetSquarePattern = new Regex(@"^(((([a-h])(3|6))|-)$)");
-            Regex halfMoveClockPattern = new Regex(@"^\d+$");
-            Regex fullMoveNumberPattern = new Regex(@"^[1-9]\d*$");
-
-            return positionPattern.IsMatch(position) &&
-                activeColorPattern.IsMatch(activeColor) &&
-                castlingAvailabilityPattern.IsMatch(castlingAvailability) &&
-                enPassantTargetSquarePattern.IsMatch(enPassantTargetSquare) &&
-                halfMoveClockPattern.IsMatch(halfMoveClock) &&
-                fullMoveNumberPattern.IsMatch(fullMoveNumber);
+            return false;
         }
-        catch (Exception ex)
-        {
-            throw ex;
-        }
+
+        string position = fenParts[0];
+        string activeColor = fenParts[1];
+        string castlingAvailability = fenParts[2];
+        string enPassantTargetSquare = fenParts[3];
+        string halfMoveClock = fenParts[4];
+        string fullMoveNumber = fenParts[5];
+
+        Regex positionPattern = new Regex(@"^([rnbqkpRNBQKP1-8]{1,8}\/){7}[rnbqkpRNBQKP1-8]{1,8}$");
+        Regex activeColorPattern = new Regex("^[bw]$");
+        Regex castlingAvailabilityPattern = new Regex(@"^((K?Q?k?q?)|-)$");
+        Regex enPassantTargetSquarePattern = new Regex(@"^(((([a-h])(3|6))|-)$)");
+        Regex halfMoveClockPattern = new Regex(@"^\d+$");
+        Regex fullMoveNumberPattern = new Regex(@"^[1-9]\d*$");
+
+        return positionPattern.IsMatch(position) &&
+            activeColorPattern.IsMatch(activeColor) &&
+            castlingAvailabilityPattern.IsMatch(castlingAvailability) &&
+            enPassantTargetSquarePattern.IsMatch(enPassantTargetSquare) &&
+            halfMoveClockPattern.IsMatch(halfMoveClock) &&
+            fullMoveNumberPattern.IsMatch(fullMoveNumber);
+
     }
 
     /// <summary>
