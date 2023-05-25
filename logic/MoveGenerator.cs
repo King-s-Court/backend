4a9198834bfdf98cc7ebcf679ff20b0cafb6b978
Tara <tarawillink@gmail.com>
2023-05-25 11:44:46 +0200

    added tasks to join game and send move

--- logic/MoveGenerator.cs
+++ logic/MoveGenerator.cs
@@ -3,7 +3,12 @@ using common.models.pieces;
 
 namespace logic;
 
-public static class MoveGenerator
+public class MoveGenerator
 {
-    
+    public string GenerateMove(int startRank, int startFile, int targetRank, int targetFile)
+    {
+        string newFen = "rnbqkbnr/ppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
+        return newFen;
+    }
 }
