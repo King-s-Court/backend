2d4627f0810f8022172bc4f2b646cd4de40cc9f6
jan.fojtik <jan.fojtik@indicia.nl>
2023-06-06 10:17:59 +0200

    fen builder with piece positions without game data

--- cli/Program.cs
+++ cli/Program.cs
@@ -9,30 +9,30 @@ string? FEN = Console.ReadLine();
 
 if (string.IsNullOrEmpty(FEN))
 {
-	Console.WriteLine("Using default FEN");
-	FENInterpreter.LoadBoardFromFEN();
-	FENInterpreter.LoadGameDataFromFEN();
-	BoardVisualizer.VisualizeBoardFromSquares();
+    Console.WriteLine("Using default FEN");
+    FENInterpreter.LoadBoardFromFEN();
+    FENInterpreter.LoadGameDataFromFEN();
+    BoardVisualizer.VisualizeBoardFromSquares();
 }
 
 else if (!FENInterpreter.IsValidFEN(FEN))
 {
-	Console.WriteLine("Invalid FEN string.");
+    Console.WriteLine("Invalid FEN string.");
 }
 
 else
 {
-	Console.WriteLine($"Using {FEN}");
-	FENInterpreter.LoadBoardFromFEN(FEN);
-	FENInterpreter.LoadGameDataFromFEN(FEN);
-	BoardVisualizer.VisualizeBoardFromSquares();
+    Console.WriteLine($"Using {FEN}");
+    FENInterpreter.LoadBoardFromFEN(FEN);
+    FENInterpreter.LoadGameDataFromFEN(FEN);
+    BoardVisualizer.VisualizeBoardFromSquares();
 }
 BoardVisualizer.VisualizeBoardForSystemCoords();
 
-Console.WriteLine("Target piece RANK: ");
+Console.WriteLine("Targeting piece @ RANK: ");
 int rank = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Target piece FILE: ");
+Console.WriteLine("Targeting piece @ FILE: ");
 int file = Convert.ToInt32(Console.ReadLine());
 
 List<(int rank, int file)> possibleOccupiedTargets = MoveValidator.PathOccupants(rank, file);
@@ -41,8 +41,10 @@ Console.WriteLine($"Path occupants for {Board.GetPiece(rank, file).AsString()}:"
 
 foreach ((int rank, int file) square in possibleOccupiedTargets)
 {
-	Console.WriteLine($"{Board.GetPiece(square.rank, square.file).AsString()} at [{square.rank}:{square.file}]");
+    Console.WriteLine($"{Board.GetPiece(square.rank, square.file).AsString()} at [{square.rank}:{square.file}]");
 }
 
-MoveValidator.GetMoveDirection(rank, file);
+Console.WriteLine($"Board as FEN string: {Board.AsFENString()}");
+
+// MoveValidator.GetMoveDirection(rank, file);
 
